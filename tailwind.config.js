1579da105909cd14dc22cb189a5f53bc2db9a0f0
adnan <sirgeant10@gmail.com>
2025-01-03 23:11:39 +0600

    Remove shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [daisyui],
+  daisyui: { themes: ["dark"] },
 };
