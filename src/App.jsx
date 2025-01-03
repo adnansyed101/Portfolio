4de41d1cbe8559b946e7456c995f285bf710bb2c
adnan <sirgeant10@gmail.com>
2025-01-03 22:46:22 +0600

    Initial commit and use Shad cn

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div className="p-4 flex ">
+      <Button className="py-1 px-4 rounded-lg">Default Button</Button>
+    </div>
+  );
+};
+
+export default App;
